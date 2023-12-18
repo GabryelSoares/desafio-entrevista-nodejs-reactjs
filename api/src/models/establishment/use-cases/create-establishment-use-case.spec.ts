496131e073edf4afd59d812f0c0c4c05204d63b6
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-18 14:20:09 -0300

    test: CreateEstablishmentUseCase

--- api/src/models/establishment/use-cases/create-establishment-use-case.spec.ts
+++ api/src/models/establishment/use-cases/create-establishment-use-case.spec.ts
@@ -1,20 +1,91 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateEstablishmentDto } from '../dto/create-establishment.dto';
+import { Establishment } from '../entities/establishment.entity';
 import { CreateEstablishmentUseCase } from './create-establishment-use-case';
+
+const establishment = new Establishment({
+  id: 1,
+  name: 'SeaPark',
+  cnpj: '00.000.000/0000-00',
+  password: 'senha',
+  address: 'test@gmail.com',
+  phone: '99 99999-9999',
+  motorcycleSlots: 10,
+  carSlots: 10,
+});
 
 describe('CreateEstablishmentUseCase', () => {
   let createEstablishmentUseCase: CreateEstablishmentUseCase;
+  let establishmentRepository: Repository<Establishment>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateEstablishmentUseCase],
+      providers: [
+        CreateEstablishmentUseCase,
+        {
+          provide: getRepositoryToken(Establishment),
+          useValue: {
+            create: jest.fn().mockReturnValue(establishment),
+            save: jest.fn().mockResolvedValue(establishment),
+          },
+        },
+      ],
     }).compile();
 
     createEstablishmentUseCase = module.get<CreateEstablishmentUseCase>(
       CreateEstablishmentUseCase,
     );
+    establishmentRepository = module.get<Repository<Establishment>>(
+      getRepositoryToken(Establishment),
+    );
   });
 
   it('should be defined', () => {
     expect(createEstablishmentUseCase).toBeDefined();
+    expect(establishmentRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should create a new establishment item successfully', async () => {
+      const createEstablishmentDto: CreateEstablishmentDto = {
+        name: 'SeaPark',
+        cnpj: '00.000.000/0000-00',
+        password: 'senha',
+        address: 'test@gmail.com',
+        phone: '99 99999-9999',
+        motorcycleSlots: 10,
+        carSlots: 10,
+      };
+      const result = await createEstablishmentUseCase.execute(
+        createEstablishmentDto,
+      );
+
+      expect(result).toEqual(establishment);
+      expect(establishmentRepository.create).toHaveBeenCalledWith(
+        createEstablishmentDto,
+      );
+      expect(establishmentRepository.create).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      const createEstablishmentDto: CreateEstablishmentDto = {
+        name: 'SeaPark',
+        cnpj: '00.000.000/0000-00',
+        password: 'senha',
+        address: 'test@gmail.com',
+        phone: '99 99999-9999',
+        motorcycleSlots: 10,
+        carSlots: 10,
+      };
+      jest
+        .spyOn(establishmentRepository, 'save')
+        .mockRejectedValueOnce(new Error('Error') as never);
+
+      expect(
+        createEstablishmentUseCase.execute(createEstablishmentDto),
+      ).rejects.toThrow('Error');
+    });
   });
 });
