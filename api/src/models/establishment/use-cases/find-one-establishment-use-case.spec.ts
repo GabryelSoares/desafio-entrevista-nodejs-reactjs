8339493fee597d868d9fc094886afb278a6f2cfc
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-18 13:34:57 -0300

    test: FindOneEstablishmentUseCase

--- api/src/models/establishment/use-cases/find-one-establishment-use-case.spec.ts
+++ api/src/models/establishment/use-cases/find-one-establishment-use-case.spec.ts
@@ -1,20 +1,67 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Establishment } from '../entities/establishment.entity';
 import { FindOneEstablishmentUseCase } from './find-one-establishment-use-case';
+
+const establishment = new Establishment({
+  id: 1,
+  name: 'SeaPark',
+  cnpj: '00.000.000/0000-00',
+  password: 'senha',
+  address: 'test@gmail.com',
+  phone: '99 99999-9999',
+  motorcycleSlots: 10,
+  carSlots: 10,
+});
 
 describe('FindOneEstablishmentUseCase', () => {
   let findOneEstablishmentUseCase: FindOneEstablishmentUseCase;
+  let establishmentRepository: Repository<Establishment>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FindOneEstablishmentUseCase],
+      providers: [
+        FindOneEstablishmentUseCase,
+        {
+          provide: getRepositoryToken(Establishment),
+          useValue: {
+            findOneBy: jest.fn().mockResolvedValue(establishment),
+          },
+        },
+      ],
     }).compile();
 
     findOneEstablishmentUseCase = module.get<FindOneEstablishmentUseCase>(
       FindOneEstablishmentUseCase,
     );
+    establishmentRepository = module.get<Repository<Establishment>>(
+      getRepositoryToken(Establishment),
+    );
   });
 
   it('should be defined', () => {
     expect(findOneEstablishmentUseCase).toBeDefined();
+    expect(establishmentRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should return a establishment item successfully', async () => {
+      const result = await findOneEstablishmentUseCase.execute(1);
+
+      expect(result).toEqual(establishment);
+      expect(establishmentRepository.findOneBy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should throw an exception', () => {
+      jest
+        .spyOn(establishmentRepository, 'findOneBy')
+        .mockRejectedValueOnce(new Error() as never);
+
+      expect(findOneEstablishmentUseCase.execute(1)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
