ae24e56dacbd72e319f7a6c6c93bb665b391eb4d
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-18 12:42:58 -0300

    feat: add FindAllEstablishmentsUseCase repository

--- api/src/models/establishment/use-cases/find-all-establishments-use-case.ts
+++ api/src/models/establishment/use-cases/find-all-establishments-use-case.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Establishment } from '../entities/establishment.entity';
 
 @Injectable()
 export class FindAllEstablishmentsUseCase {
-  execute() {
-    return `This action returns all establishment`;
+  constructor(
+    @InjectRepository(Establishment)
+    private readonly establishmentRepository: Repository<Establishment>,
+  ) {}
+  async execute() {
+    return await this.establishmentRepository.find({ order: { name: 'ASC' } });
   }
 }
