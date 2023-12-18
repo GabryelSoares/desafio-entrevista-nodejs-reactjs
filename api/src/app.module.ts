01fa4457511aecd44e22e819ac3c59f9c031ddb8
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-17 22:48:31 -0300

    fix: move establishment module to models path

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EstablishmentModule } from './establishment/establishment.module';
+import { EstablishmentModule } from './models/establishment/establishment.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
