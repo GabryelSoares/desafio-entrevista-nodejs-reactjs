878096f7e2d2f60c1bf3ddc3c7710b50d6ec821c
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-18 20:21:17 -0300

    feat: add UpdateEstablishmentUseCase repository

--- api/src/models/establishment/use-cases/update-establishment-use-case.ts
+++ api/src/models/establishment/use-cases/update-establishment-use-case.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UpdateEstablishmentDto } from '../dto/update-establishment.dto';
+import { Establishment } from '../entities/establishment.entity';
+import { FindOneEstablishmentUseCase } from './find-one-establishment-use-case';
 
 @Injectable()
 export class UpdateEstablishmentUseCase {
-  execute(id: number, updateEstablishmentDto: UpdateEstablishmentDto) {
-    console.log('updateEstablishmentDto:: ', updateEstablishmentDto);
-    return `This action updates a #${id} establishment`;
+  constructor(
+    @InjectRepository(Establishment)
+    private readonly establishmentRepository: Repository<Establishment>,
+    private readonly findOneEstablishmentUseCase: FindOneEstablishmentUseCase,
+  ) {}
+
+  async execute(id: number, updateEstablishmentDto: UpdateEstablishmentDto) {
+    const establishment = await this.findOneEstablishmentUseCase.execute(id);
+
+    const result = this.establishmentRepository.merge(
+      establishment,
+      updateEstablishmentDto,
+    );
+    return await this.establishmentRepository.save(result);
   }
 }
