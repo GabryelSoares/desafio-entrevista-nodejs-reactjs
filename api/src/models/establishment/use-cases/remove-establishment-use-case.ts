c91c956605938273b108219490c293f27059e050
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-18 21:00:19 -0300

    feat: add RemoveEstablishmentUseCase repository

--- api/src/models/establishment/use-cases/remove-establishment-use-case.ts
+++ api/src/models/establishment/use-cases/remove-establishment-use-case.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Establishment } from '../entities/establishment.entity';
+import { FindOneEstablishmentUseCase } from './find-one-establishment-use-case';
 
 @Injectable()
 export class RemoveEstablishmentUseCase {
-  execute(id: number) {
-    return `This action removes a #${id} establishment`;
+  constructor(
+    @InjectRepository(Establishment)
+    private readonly establishmentRepository: Repository<Establishment>,
+    private readonly findOneEstablishmentUseCase: FindOneEstablishmentUseCase,
+  ) {}
+  async execute(id: number) {
+    await this.findOneEstablishmentUseCase.execute(id);
+    return await this.establishmentRepository.delete(id);
   }
 }
